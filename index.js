36d425748a42fcb05f646e0783786da0ead539a8
federica100 <105656784+federica100@users.noreply.github.com>
2022-06-21 12:50:01 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function showCookie() {
+  let cookie = document.querySelector("#cookie");
+  cookie.style = "display:block;";
+}
+
+function hideCookie() {
+  let cookie = document.querySelector("#cookie");
+  cookie.style = "display:none;";
+}
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
